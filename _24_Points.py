2659f27e12d3489aeb348ba40deb3f3a96f7235c
霄 杨 <AEO999@outlook.com>
2020-10-02 18:22:13 +0800

    Add "time.sleep()".

--- _24_Points.py
+++ _24_Points.py
@@ -1,16 +1,21 @@
 import random
+import time
 
 while 'y' in input("Play 24 Points? (Y/N)\n").lower():
+    time.sleep(1)
     a = random.randint(1, 13)
     b = random.randint(1, 13)
     c = random.randint(1, 13)
     d = random.randint(1, 13)
     print("The four numbers are:", a, b, c, d)
+    time.sleep(1)
     print("You have three tries.")
+    time.sleep(1)
     times = 0
     flag = True
     while flag and times < 3:
         expr = input("Your expression: ")
+        time.sleep(1)
         if str(a) in expr and str(b) in expr and str(c) in expr and str(d) in expr and eval(expr) == 24:
             flag = False
         else:
@@ -21,4 +26,6 @@ while 'y' in input("Play 24 Points? (Y/N)\n").lower():
     else:
         print("Sorry, no more tries. Try harder next time. (Or maybe it has no solution.)")
     print("")
+    time.sleep(1)
+time.sleep(1)
 print("Good bye.")
